af5c15a652fbb5459466e131adeaf35cbbbbb93a
Dale Stammen <dalestam@microsoft.com>
2014-11-08 18:17:00 -0800

    removed unused files for step 2 button

--- Classes/GameConfig.h
+++ Classes/GameConfig.h
@@ -20,40 +20,11 @@
 #define SCREEN_HEIGHT 480
 #define DISPLAY_STATS true
 
-#define K_NUM_CLOUDS 12
-#define K_MIN_PLATFORM_STEP 50
-#define K_MAX_PLATFORM_STEP 300
-#define K_NUM_PLATFORMS 15
-#define K_PLATFORM_TOP_PADDING 10
-#define K_MAX_PLATFORMS_IN_GAME 100
-
-#define K_MIN_BONUS_STEP 30
-#define K_MAX_BONUS_STEP 50
-
-#define K_PLAY_BACKGROUND_MUSIC 1
-#define K_PLAY_BACKGROUND_MUSIC_VOLUME 0.2f
-#define K_PLAY_SOUND_EFFECTS 1
-
 
 enum {
     kSpriteManager = 0,
-    kRocketMan,
-    kScoreLabel,
-    kExit,
-    kCloudsStartTag = 1000,
-    kPlatformsStartTag = 2000,
-    kBonusStartTag = 3000,
-    kPopUpPlayerTag = 10000,
-    kPopUpEditBoxTag = 150000
 };
 
-enum
-{
-    kBonus5 = 0,
-    kBonus10,
-    kBonus50,
-    kBonus100,
-    kNumBonuses
-};
+
 
 #endif
