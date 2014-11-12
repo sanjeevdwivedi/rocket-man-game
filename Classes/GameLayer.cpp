86334e16c94440fff6a4092fc4cdd6102d8369bd
Sanjeev Dwivedi <sanjeev.dwivedi@gmail.com>
2014-11-12 01:00:01 -0800

    Making the platforms Move as the Rocketman jumps up

--- Classes/GameLayer.cpp
+++ Classes/GameLayer.cpp
@@ -115,13 +115,6 @@ void GameLayer::update(float dt)
     rm_velocity.y += rm_acceleration.y * dt;
     rm_position.y += rm_velocity.y * dt;
 
-    if (rm_position.y > SCREEN_HEIGHT * 0.5f)
-    {
-        float delta = rm_position.y - SCREEN_HEIGHT * 0.5f;
-        rm_position.y = SCREEN_HEIGHT * 0.5f;
-    }
-
-
     // Add the collision logic between the rocketman and the struss/asteroid. The rocketman collides only 
     // when he is falling down.
     int platformTag;
@@ -141,8 +134,38 @@ void GameLayer::update(float dt)
             if (rm_position.x > max_x && rm_position.x < min_x &&
                 rm_position.y > platform_position.y && rm_position.y < min_y)
                 _jump();
+
+            if (rm_position.y < -rm_size.height)
+            {
+                // TODO: (exit the game here)
+            }
         }
 
+    }
+    else if (rm_position.y > SCREEN_HEIGHT * 0.5f)
+    {
+        float delta = rm_position.y - SCREEN_HEIGHT * 0.5f;
+        rm_position.y = SCREEN_HEIGHT * 0.5f;
+        currentPlatformY -= delta;
+
+        for (platformTag = kPlatformsStartTag; platformTag < kPlatformsStartTag + K_NUM_PLATFORMS; platformTag++)
+        {
+            CCSprite* platform = dynamic_cast<CCSprite*>(batchNode->getChildByTag(platformTag));
+            CCPoint position = platform->getPosition();
+            position = ccp(position.x, position.y - delta);
+
+            // If the platform just became invisible, reset it to just above the screen
+            if (position.y < -platform->getContentSize().height * 0.5f)
+            {
+                currentPlatformTag = platformTag;
+                _resetPlatform();
+            }
+            else
+            {
+                // If the platform is still visible, decrease its Y coordinates so it looks like the scene is scrolling
+                platform->setPosition(position);
+            }
+        }
     }
 
     // draw RocketMan at its new position
